a5a86247e36d96468b23fd7c6ea8adc10b564b29
Alessandro Gentilini <alessandro.gentilini@gmail.com>
2015-12-28 20:23:51 +0100

    The plot thickens.

--- bot.R
+++ bot.R
@@ -8,15 +8,43 @@ extract_info <- function(file_name)
   for ( i in seq(1,10) ){
     name_xpath = paste('//*[@id="templ-housinglist"]/div[2]/div/section/ul/li[',i,sep='')
     name_xpath = paste(name_xpath,']/section[2]/a',sep='')
-    local_name=c(local_name,xpathSApply(doc,name_xpath,xmlValue))
+    x<-xpathSApply(doc,name_xpath,xmlValue)
     
-    email_xpath = paste('//*[@id="templ-housinglist"]/div[2]/div/section/ul/li[',i,sep='')
-    email_xpath = paste(email_xpath,']/section[3]/p/a[2]',sep='')
-    x<-xpathSApply(doc,email_xpath,xmlValue)
-    if(is.null(x) || length(x)==0 || x==''){
-      x='no_mail'
-      print('************************NO MAIL***************')
+    no_description = FALSE
+    if(length(x)==0){
+      no_description = TRUE
+      name_xpath = paste('//*[@id="templ-housinglist"]/div[2]/div/section/ul/li[',i,sep='')
+      name_xpath = paste(name_xpath,']/section[2]',sep='')
+      x<-xpathSApply(doc,name_xpath,xmlValue)     
+      x<-strsplit(x,'\n')[[1]][2]
     }
+    local_name=c(local_name,x)
+
+
+    email_xpath_1 = paste('//*[@id="templ-housinglist"]/div[2]/div/section/ul/li[',i,sep='')
+    email_xpath_1 = paste(email_xpath_1,']/section[3]/p/a[1]',sep='')
+    x1<-xpathSApply(doc,email_xpath_1,xmlValue)
+    if(is.null(x1) || length(x1)==0 || x1==''){
+      x1='no_mail'
+    }    
+    
+    email_xpath_2 = paste('//*[@id="templ-housinglist"]/div[2]/div/section/ul/li[',i,sep='')
+    email_xpath_2 = paste(email_xpath_2,']/section[3]/p/a[2]',sep='')
+    x2<-xpathSApply(doc,email_xpath_2,xmlValue)
+    if(is.null(x2) || length(x2)==0 || x2==''){
+      x2='no_mail'
+    }        
+    
+    if ( '@' %in% strsplit(x1,'')[[1]] ) {
+      x=x1
+    } else {
+      if ( '@' %in% strsplit(x2,'')[[1]] ) {
+        x=x2
+      } else {
+        x='unexpected'
+      }
+    }
+
     local_email = c(local_email,x)
   }
   if(length(local_name)==length(local_email)){
@@ -32,7 +60,7 @@ extract_info <- function(file_name)
 name=c()
 email=c()
 
-for ( j in seq(0,100,10) ) {
+for ( j in seq(0,1536,10) ) {
   cl_options <- paste('--post-data "search=1&feratelSort=3&feratelOffset=',j,sep='')
   cl_options <- paste(cl_options,'"',sep='')
   file_name = paste('f_',j,sep='')
