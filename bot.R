f126b1697a9326867a67e94dc06b092a75172e00
Alessandro Gentilini <alessandro.gentilini@gmail.com>
2015-12-28 17:21:47 +0100

    Fix some issues regarding dataframe and vector.

--- bot.R
+++ bot.R
@@ -26,21 +26,21 @@ extract_info <- function(file_name)
     write.table(df,file=file_name, row.names = FALSE, sep=",", quote=FALSE)  
   }
 
-  return(c(local_name,local_email))
+  return(data.frame(name=local_name,email=local_email))
 }
 
 name=c()
 email=c()
 
-for ( j in seq(0,1534,10) ) {
+for ( j in seq(0,100,10) ) {
   cl_options <- paste('--post-data "search=1&feratelSort=3&feratelOffset=',j,sep='')
   cl_options <- paste(cl_options,'"',sep='')
   file_name = paste('f_',j,sep='')
   file_name = paste(file_name,'.html',sep='')
   download.file('http://www.oetztal.com/accommodation-list-winter#searchfilter',file_name,'wget',extra=cl_options)
   r <- extract_info(file_name)
-  name=c(name,r[1])
-  email=c(email,r[2])
+  name=c(name,as.vector(r$name))
+  email=c(email,as.vector(r$email))
 }
 
 df<-data.frame(name=name,email=email)
